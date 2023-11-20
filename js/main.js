2fe30786b00191513cfbef35be1d05ef31fed868
SimoneCalzari <simone.calzari@outlook.it>
2023-11-20 18:19:41 +0100

    feat: milestone 0 e 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+// MILESTONE O
+
+const team = [
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    imgNome: 'wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    imgNome: 'angela-caroll-chief-editor.jpg'
+  },
+  {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    imgNome: 'walter-gordon-office-manager.jpg'
+  },
+  {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    imgNome: 'angela-lopez-social-media-manager.jpg'
+  },
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Developer',
+    imgNome: 'scott-estrada-developer.jpg'
+  },
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Graphic Designer',
+    imgNome: 'barbara-ramos-graphic-designer.jpg'
+  },
+];
+
+console.log(team);
+
+// MILESTONE 1
+for (let i = 0; i < team.length; i++) {
+  for (let key in team[i]) {
+    console.log(`${key}:`,team[i][key]);
+  }
+}
